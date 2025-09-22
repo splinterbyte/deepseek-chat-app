fa080f51c2023f645e7c7d22642e39497307539d
splinterbyte <abramchuk141094@gmail.com>
2025-09-22 22:51:09 +0300

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,7 +38,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:3001/api/chat', {
+      const response = await fetch('/api/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ messages: newMessages }),
